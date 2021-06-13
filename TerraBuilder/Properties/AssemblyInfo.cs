1dea1414d7e2705834034c5a7c57c036f8ae0671
Miyabi-ill <44920054+Miyabi-ill@users.noreply.github.com>
2021-06-13 21:51:36 +0900

    バージョン番号を0.0.2に

--- TerraBuilder/Properties/AssemblyInfo.cs
+++ TerraBuilder/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Windows;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("TUBGWorldGenerator")]
+[assembly: AssemblyTitle("TerraBuild")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
@@ -49,5 +49,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.1")]
-[assembly: AssemblyFileVersion("0.0.1")]
+[assembly: AssemblyVersion("0.0.2")]
+[assembly: AssemblyFileVersion("0.0.2")]
