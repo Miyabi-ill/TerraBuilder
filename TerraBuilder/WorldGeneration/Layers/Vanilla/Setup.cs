4e42f83c32b0f357538e85d4e45e4191b98a9096
Miyabi-ill <kodamamiyabi@gmail.com>
2022-01-06 00:05:41 +0900

    バニラ生成のプレースホルダーを追加

---
+++ TerraBuilder/WorldGeneration/Layers/Vanilla/Setup.cs
@@ -0,0 +1,120 @@
+﻿// Copyright (c) Miyabi. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace TerraBuilder.WorldGeneration.Layers.Vanilla
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class Setup : IWorldGenerationLayer
+    {
+        /* バニラ生成名とレイヤー名対応表（仮置き）
+         * Reset -> Setup
+         * Terrain -> Terrain
+         * Dunes -> Dunes
+         * Ocean Sand -> OceanSand
+         * Sand Patches -> SandPatches
+         * Tunnels -> Tunnels
+         * Dirt Wall Backgrounds -> DirtWallBackgrounds
+         * Clay -> Clay
+         * Small Holes -> SmallHoles
+         * Dirt Layer Caves -> DirtLayerCaves
+         * Rock Layer Caves -> RockLayerCaves
+         * Surface Caves -> SurfaceCaves
+         * Wavy Caves -> WavyCaves
+         * Generate Ice Biome -> IceBiome
+         * Grass -> Grass
+         * Jungle -> Jungle
+         * Mud Caves To Grass -> MudCavesToGrass
+         * Full Desert -> FullDesert
+         * Floating Islands -> FloatingIslands
+         * Mushroom Patches -> MushroomPatches
+         * Marble -> Marble
+         * Granite -> Granite
+         * Dirt To Mud -> DirtToMud
+         * Silt -> Silt
+         * Shinies -> Shinies
+         * Webs -> Webs
+         * Underworld -> Underworld
+         * Corruption -> Corruption
+         * Lakes -> Lakes
+         * Dungeon -> Dungeon
+         * Slush -> Slush
+         * Mountain Caves -> MountainCaves
+         * Beaches -> Beaches
+         * Gems -> Gems
+         * Gravitating Sand -> GravitateSand
+         * Create Ocean Caves -> OceanCaves
+         * Clean Up Dirt -> CleanUpDirt
+         * Pyramids -> Pyramids
+         * Dirt Rock Wall Runner -> DirtRockWalls
+         * Living Trees -> LivingTrees
+         * Wood Tree Walls -> WoodTreeWalls
+         * Altars -> Altars
+         * Wet Jungle -> WetJungle
+         * Jungle Temple -> JungleTemple
+         * Hives -> Hives
+         * Jungle Chests -> JungleChests
+         * Settle Liquids -> SettleLiquids
+         * Remove Water From Sand -> RemoveWaterFromSand
+         * Oasis -> Oasis
+         * Shell Piles -> ShellPiles
+         * Smooth World -> SmoothWorld
+         * Waterfalls -> Waterfalls
+         * Ice -> Ice
+         * Wall Variety -> WallVariety
+         * Life Crystals -> LifeCrystals
+         * Statues -> Statues
+         * Buried Chests -> BuriedChests
+         * Surface Chests -> SurfaceChests
+         * Jungle Chests Placement -> JungleChestsPlacement
+         * Water Chests -> WaterChests
+         * Spider Caves -> SpiderCaves
+         * Gem Caves -> GemCaves
+         * Moss -> Moss
+         * Temple -> Temple
+         * Cave Walls -> CaveWalls
+         * Jungle Trees -> JungleTrees
+         * Floating Island Houses -> FloatingIslandHouses
+         * Quick Cleanup -> QuickCleanup
+         * Pots -> Pots
+         * Hellforge -> Hellforge
+         * Spreading Grass -> SpreadGrass
+         * Surface Ore and Stone -> SurfaceOreAndStone
+         * Place Fallen Log -> FallenLog
+         * Traps -> Traps
+         * Piles -> Piles
+         * Spawn Point -> SpawnPoint
+         * Grass Wall -> GrassWall
+         * Guide -> Guide
+         * Sunflowers -> Sunflowers
+         * Planting Trees -> PlantTrees
+         * Herbs -> Herbs
+         * Dye Plants -> DyePlants
+         * Webs and Honey -> WebsAndHoney
+         * Weeds -> Weeds
+         * Glowing Mushrooms and Jungle Plants -> GlowingMushroomsAndJunglePlants
+         * Jungle Plants -> JunglePlants
+         * Vines -> Vines
+         * Flowers -> Flowers
+         * Mushrooms -> Mushrooms
+         * Gems In Ice Biome -> GemsInIceBiome
+         * Random Gems -> RandomGems
+         * Moss Grass -> MossGrass
+         * Muds Walls In Jungle -> MudsWallsInJungle
+         * Larva -> Larva
+         * Settle Liquids Again -> SettleLiquidsAgain
+         * Cactus, Palm Trees & Coral -> SandPlants
+         * Tile Cleanup -> TileCleanup
+         * Lihzahrd Altars -> LihzahrdAltars
+         * Micro Biomes -> MicroBiomes
+         * Water Plants -> WaterPlants
+         * Stalac -> Stalac
+         * Remove Broken Traps -> RemoveBrokenTraps
+         * Final Cleanup -> FinalCleanup
+         */
+    }
+}
---
+++ TerraBuilder/WorldGeneration/Layers/Vanilla/WebsAndHoney.cs
@@ -0,0 +1,12 @@
+﻿namespace TerraBuilder.WorldGeneration.Layers.Vanilla
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    internal class WebsAndHoney
+    {
+    }
+}
